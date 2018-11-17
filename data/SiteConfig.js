3a8bd612106ab1cc34cf12c45e5ffe2bca0bb012
Julius <julius.vering@berkeley.edu>
2018-11-17 14:14:36 -0800

    Initial commit

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,8 +3,8 @@ module.exports = {
   siteTitleShort: "JV", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "Julius Vering Personal Website", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://juliusvering.github.io", // Domain of your website without pathPrefix.
-  pathPrefix: "/julius-vering", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  siteUrl: "https://www.juliusvering.com", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: "Julius Vering's personal website.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
